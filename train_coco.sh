32b644ac4c1c91402f7eff3841aca6b5e6bd0723
hongrui16 <hongrui_hust@foxmail.com>
2021-09-26 17:20:33 +0800

    delete train_coco.sh train_vos.sh

--- train_coco.sh
+++
@@ -1,3 +0,0 @@
-python train.py --backbone mobilenet --lr 0.01 --workers 4 --epochs 40 --batch-size 128 --gpu-ids 0,1  --eval-interval 1 --dataset coco
-# python train.py --backbone mobilenet --lr 0.01 --workers 4 --epochs 40 --batch-size 2 --checkname deeplab-resnet --eval-interval 1 --dataset coco --no-cuda
-
