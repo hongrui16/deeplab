6eedfff6669ca4b63f55f24ff12110dfdab2c9cf
hongrui16 <hongrui_hust@foxmail.com>
2021-10-13 19:51:49 +0800

    edit relocate img regin

--- tools/util.py
+++ tools/util.py
@@ -4,6 +4,7 @@ import os
 import numpy as np
 import random
 import shutil
+import cv2
 
 def write_list_to_txt(txt_filepath, lists):
     f = open(txt_filepath, "a+")
@@ -20,6 +21,25 @@ def read_txt_to_list(txt_filepath):
     # print(len(lists))
     return lists
 
+def sort_left_right_lane(label):
+    left_0 = min(label[label>0])
+    # print(label[label>left_0].min())
+    right_0 = 2*left_0
+    # print('left_0 ',left_0, 'right_0 ', right_0)
+    return label*(label==left_0), label*(label==right_0)
+
+def find_bottom_lane_location_in_labels(label):
+    left_lane, right_lane = sort_left_right_lane(label.copy())
+    # cv2.imwrite('left_lane.png', left_lane)
+    # cv2.imwrite('right_lane.png', right_lane)
+    assert label.ndim == 2
+    h, w = label.shape
+    max_y_left = left_lane.nonzero()[0].max()
+    max_y_right = right_lane.nonzero()[0].max()
+    max_y = min(max_y_left, max_y_right)
+    left_x_pos = left_lane[max_y-20:max_y].nonzero()[1].mean()
+    right_x_pos = right_lane[max_y-50:max_y].nonzero()[1].mean()
+    return left_x_pos, right_x_pos
 
 if __name__ == '__main__':
     read_txt_to_list('/home/hongrui/project/metro_pro/edge_detection/chdis_v2.txt')
